b4451a1c91bc995f5fe9cea7122e096da72cb9bc
ricvon <ricardo.santo@gmail.com>
2026-02-20 15:40:06 -0400

    Aula 16. Criar a URL da imagem e configurando o tamanho do Upload

--- src/main/java/br/com/ricvon/imageliteapi/application/images/ImagesController.java
+++ src/main/java/br/com/ricvon/imageliteapi/application/images/ImagesController.java
@@ -2,19 +2,19 @@ package br.com.ricvon.imageliteapi.application.images;
 
 
 import br.com.ricvon.imageliteapi.domain.entity.Image;
-import br.com.ricvon.imageliteapi.domain.enums.ImageExtension;
 import br.com.ricvon.imageliteapi.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,7 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ImagesController {
     private final ImageService service;
-
+    private final ImageMapper mapper;
     //{"name": "", "size":100}//formato application/json
 
     //para receber arquivo de upload é necessário usar
@@ -39,7 +39,7 @@ public class ImagesController {
             /*log.info("Content Type: {}", file.getContentType());
             log.info("Media Type: {}", MediaType.valueOf(file.getContentType()));
             log.info("Nome definido por a imagem: {}", name);
-            log.info("Tags> {}", tags);*/
+            log.info("Tags> {}", tags);
 
         Image image = Image.builder()
                 .name(name)
@@ -47,10 +47,23 @@ public class ImagesController {
                 .size(file.getSize())
                 .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
                 .file(file.getBytes())
-                .build();
+                .build();*/
 
-        service.save(image);
+        Image image = mapper.mapToImage(file, name, tags);
 
-        return ResponseEntity.ok().build();
+        Image sevedImage = service.save(image);
+
+        URI imageUri = buildImageURL(sevedImage);
+
+         return ResponseEntity.created(imageUri).build();
+    }
+
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build()
+                .toUri();
     }
 }
