7135c004b4ab8a9dc1f4a4c780a4a54b97be48d9
ricvon <ricardo.santo@gmail.com>
2026-02-21 10:42:47 -0400

    Aula 20. Adicionando capacidade de executar Specifications no repositório de imagens

--- src/main/java/br/com/ricvon/imageliteapi/infra/repository/ImageRepository.java
+++ src/main/java/br/com/ricvon/imageliteapi/infra/repository/ImageRepository.java
@@ -1,7 +1,14 @@
 package br.com.ricvon.imageliteapi.infra.repository;
 
 import br.com.ricvon.imageliteapi.domain.entity.Image;
+import br.com.ricvon.imageliteapi.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+    default List<Image> findByExtensionAndNameorTagsLike(ImageExtension extension, String query){
+        return findAll();
+    }
 }
